80b6995b74a427ac4103d7877496ce2b7fbd9e45
norm02 <richa0920@gmail.com>
2023-01-24 20:03:33 +0900

    station2_commit2

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,17 +1,17 @@
-data = "global data";
+data = 'global data'
 
 // このreturnDataに代入される関数の形をアロー関数に変えて"global data"が表示されるようにしてください
 // ↓関数の中の処理は変更しないでください
 const returnThisData = function () {
-  return this.data;
-};
+  return this.data
+}
 // ↑関数の中の処理は変更しないでください
 
 const object = {
-  data: "object data",
+  data: 'object data',
   func: returnThisData,
-};
+}
 
 function getData() {
-  return object.func();
+  return object.func()
 }
--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -4,8 +4,14 @@ async function getData() {
   // 最初から表示されていると、クリアになりません。
   const userList = [
     { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
-  ];
+    {
+      id: 2,
+      first_name: '太郎',
+      family_name: '山田',
+      affilication: 'HogeHoge大学',
+      is_student: true,
+    },
+  ]
   const result = await test()
   return await result
 }
@@ -13,4 +19,3 @@ async function getData() {
 function test() {
   return
 }
-
