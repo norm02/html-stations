f939b9943371d2595a67a78cffb6fb0ed2f1c48b
bono <62996844+norm02@users.noreply.github.com>
2023-01-25 13:32:28 +0900

    station15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -1,21 +1,38 @@
 async function getData() {
-  // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
-  // ３秒後にフルネームのリストを表示されるようにしましょう。
-  // 最初から表示されていると、クリアになりません。
-  const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
-    {
-      id: 2,
-      first_name: '太郎',
-      family_name: '山田',
-      affilication: 'HogeHoge大学',
-      is_student: true,
-    },
-  ]
-  const result = await test()
-  return await result
-}
+  const result = await new Promise((resolve) => {
+    setTimeout(() => {
+      const userList = [
+        {
+          id: 1,
+          first_name: '優',
+          family_name: '大木',
+          affilication: 'TechTrain',
+          is_student: false,
+        },
+        {
+          id: 2,
+          first_name: '太郎',
+          family_name: '山田',
+          affilication: 'HogeHoge大学',
+          is_student: true,
+        },
+      ]
+      resolve(userList)
+    }, 3000)
+  })
+  return result.map(buildFullName)
 
-function test() {
-  return
+  function buildFullName(data) {
+    var expect = new Object()
+    let full_name_expect = data.family_name + ' ' + data.first_name
+
+    expect.id = data.id
+    expect.first_name = data.first_name
+    expect.family_name = data.family_name
+    expect.full_name = full_name_expect
+    expect.affilication = data.affilication
+    expect.is_student = data.is_student
+    return expect
+  }
 }
+getData()
